4f9ecbfda5591f92b2595548fe5c804171a2fc51
Aisha Agarwal <78947668+aisha09agarwal@users.noreply.github.com>
2024-03-11 13:32:59 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 200308
+let accountEmail = "aisha@gmail.com"
+var accountPassword = "12345"
+accountCity = "Durgapur"
+let accountState;
+
+//accountId=2 //not allowed
+
+/*
+prefer not to use var
+for problem in block scope and functional scope
+*/
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
